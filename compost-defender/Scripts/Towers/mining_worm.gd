162b1a01cb595ae9001317f47b22d0208c9620ba
ecclepe-droid <ecclepe@whs.school.nz>
2026-07-31 13:17:03 +1200

    the mining worm can attack
    also added a test enemy

--- compost-defender/Scripts/Towers/mining_worm.gd
+++ compost-defender/Scripts/Towers/mining_worm.gd
@@ -8,4 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+
+	
+	if $MiningWormAttackTimer.time_left <= 0.5:
+		print("Swinging pickaxe!")
+		$MiningWormAttackTimer.start(1.5)
+		
+		var aim_direction = global_position.direction_to(get_global_mouse_position())
+		var aim_angle = rad_to_deg(aim_direction.angle())
+		print(aim_angle)
