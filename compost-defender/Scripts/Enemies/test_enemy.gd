cb4b75afb2d40f31a7d035d0c8b804fb75056630
taumois <238574759+taumois@users.noreply.github.com>
2026-07-31 14:56:49 +1200

    Add Enemy Health Logic

--- compost-defender/Scripts/Enemies/test_enemy.gd
+++ compost-defender/Scripts/Enemies/test_enemy.gd
@@ -1,11 +1,24 @@
 extends PathFollow2D
 
 const SPEED = 999
+const MAX_HEALTH = 100
+var health
 
 
 func _ready() -> void:
+	health = MAX_HEALTH
 	progress = 0
 
 
 func _process(delta: float) -> void:
 	progress += SPEED * delta
+
+
+func damage(damageAmount: int) -> void:
+	health -= damageAmount
+	if health <= 0:
+		_die()
+
+
+func _die() -> void:
+	queue_free()
