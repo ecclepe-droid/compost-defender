2d44724028b56fc3b1ed3b3ea99602bea92141b8
taumois <238574759+taumois@users.noreply.github.com>
2026-07-31 13:08:15 +1200

    Update level_select_screen.gd

--- compost-defender/Scripts/level_select_screen.gd
+++ compost-defender/Scripts/level_select_screen.gd
@@ -10,8 +10,8 @@ const LEVEL_FIVE_PACKED = ("res://Scenes/level_1.tscn")
 
 func _on_level_one_button_pressed() -> void:
 	get_tree().change_scene_to_packed(LEVEL_ONE_PACKED)
+	printerr("Level One is not finished yet")
 
 
 func _on_start_screen_button_pressed() -> void:
 	get_tree().change_scene_to_file(STARTING_SCREEN_FILE)
-	printerr("Level One is not finished yet")
