686d290ff7314b65569416995177b739d955ead2
taumois <238574759+taumois@users.noreply.github.com>
2026-08-14 15:09:12 +1200

    Fix Darthworm Bug

--- compost-defender/Scripts/Towers/darth_worm.gd
+++ compost-defender/Scripts/Towers/darth_worm.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 func _ready() -> void:
-	var attack = $LightsaberAttack
+	var attack = $MeleeAttack
 	attack.damage = 100
 	attack.cooldownInSeconds = 2.5
 	attack.set_scale(Vector2.ONE * 1.7)
