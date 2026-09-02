f03423aba68c821dc91e2c4f4e1ac111daa8854f
taumois <238574759+taumois@users.noreply.github.com>
2026-09-03 09:17:11 +1200

    Improve Wave Making

--- compost-defender/Scripts/wave.gd
+++ compost-defender/Scripts/wave.gd
@@ -3,8 +3,3 @@ class_name Wave
 extends Node
 
 var enemyScenes: Array[PackedScene]
-var secsAfterPreviousWave: float
-
-func _init(enemyScenes: Array[PackedScene], secsAfterPrevoiusWave: float) -> void:
-	self.enemyScenes = enemyScenes
-	self.secsAfterPreviousWave = secsAfterPreviousWave
