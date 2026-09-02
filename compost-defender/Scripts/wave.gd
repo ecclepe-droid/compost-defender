6926d8930060e14a5de1bca3506d9d767aaf1a9b
taumois <238574759+taumois@users.noreply.github.com>
2026-09-03 08:48:40 +1200

    Mis spelt variable in wave.gd

--- compost-defender/Scripts/wave.gd
+++ compost-defender/Scripts/wave.gd
@@ -5,6 +5,6 @@ extends Node
 var enemyScenes: Array[PackedScene]
 var secsAfterPreviousWave: float
 
-func _init(enemyScenes: Array[PackedScene], secsAfterPrevoiusScene: float) -> void:
+func _init(enemyScenes: Array[PackedScene], secsAfterPrevoiusWave: float) -> void:
 	self.enemyScenes = enemyScenes
 	self.secsAfterPreviousWave = secsAfterPreviousWave
