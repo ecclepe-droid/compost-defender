1a0f411b2b42a064e3e5126d0b39948123930ea1
ecclepe-droid <ecclepe@whs.school.nz>
2026-09-11 14:23:25 +1200

    Ui is changed

--- compost-defender/Scripts/level_1.gd
+++ compost-defender/Scripts/level_1.gd
@@ -8,7 +8,7 @@ var enemy_manager: Node2D
 
 func _ready() -> void:
 	enemy_manager = $EnemyManager
-	Globals.set_compost_amount(0)
+	Globals.set_compost_amount(50)
 	Globals.compost_integrity = 20
 	user_interface = $CanvasLayer/InLevelUI
 	fade_out()
@@ -33,19 +33,19 @@ func _ready() -> void:
 func _enemy_packed_from_name(enemy_name: String):
 	var enemy_file_path = "res://Scenes/Enemies/" + enemy_name.to_snake_case() + ".tscn"
 	var packed_scene_of_enemy = load(enemy_file_path)
-	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 2.5))
+	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 1))
 	enemy_name = "apple"
 	enemy_file_path = "res://Scenes/Enemies/" + enemy_name.to_snake_case() + ".tscn"
 	packed_scene_of_enemy = load(enemy_file_path)
-	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 2.5))
+	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 1))
 	enemy_name = "banana"
 	enemy_file_path = "res://Scenes/Enemies/" + enemy_name.to_snake_case() + ".tscn"
 	packed_scene_of_enemy = load(enemy_file_path)
-	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 2.5))
+	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 1))
 	enemy_name = "cashew"
 	enemy_file_path = "res://Scenes/Enemies/" + enemy_name.to_snake_case() + ".tscn"
 	packed_scene_of_enemy = load(enemy_file_path)
-	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 2.5))
+	enemy_manager.request_wave(Wave.create([packed_scene_of_enemy], 1))
 	return packed_scene_of_enemy
 
 
